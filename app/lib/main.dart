9b744f8fde2ecbbeb9a768c92fb183abbc1b1cc1
Angelo Breuer <angelobr@outlook.de>
2023-03-13 21:50:23 +0100

    feat: Show month in home view

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -48,6 +48,35 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  _getNameOfMonth(int month) {
+    switch (month) {
+      case 1:
+        return "Januar";
+      case 2:
+        return "Februar";
+      case 3:
+        return "März";
+      case 4:
+        return "April";
+      case 5:
+        return "Mai";
+      case 6:
+        return "Juni";
+      case 7:
+        return "Juli";
+      case 8:
+        return "August";
+      case 9:
+        return "September";
+      case 10:
+        return "Oktober";
+      case 11:
+        return "November";
+      case 12:
+        return "Dezember";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             Text(
-              _getDayOfWeek(DateTime.now().weekday),
+              "${_getDayOfWeek(DateTime.now().weekday)}, ${DateTime.now().day}. ${_getNameOfMonth(DateTime.now().month)} ${DateTime.now().year}",
               style: TextStyle(fontSize: 20, color: Colors.grey.shade400),
             ),
             SizedBox(height: 12),
